8e1985e985f3509c135a4fbb70e418b99ec98723
Will Jobs <7775919+willjobs@users.noreply.github.com>
2026-01-03 17:06:56 -0500

    Add cache headers to prevent stale service workers from Cloudflare

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * Uses a cache-first strategy for static assets.
  */
 
-const CACHE_VERSION = "v83-426b21a";
+const CACHE_VERSION = "v84-4a0faa0";
 const STATIC_CACHE_NAME = `sunshine-optimist-static-${CACHE_VERSION}`;
 
 // Static assets to cache on install
