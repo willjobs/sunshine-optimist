4a0faa0ab2c6baf9b96dcb502af1029155d39c12
Will Jobs <7775919+willjobs@users.noreply.github.com>
2026-01-03 17:02:42 -0500

    Deleted useless tests

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * Uses a cache-first strategy for static assets.
  */
 
-const CACHE_VERSION = "v81-da0814a";
+const CACHE_VERSION = "v83-426b21a";
 const STATIC_CACHE_NAME = `sunshine-optimist-static-${CACHE_VERSION}`;
 
 // Static assets to cache on install
