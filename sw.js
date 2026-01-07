9cedffbf2a4cfd4373c43dad7aea5d298800dc74
Will Jobs <7775919+willjobs@users.noreply.github.com>
2026-01-07 11:14:44 -0500

    Removed social media share buttons due to limitations with how the companies let you post (or not post). Expanded clipboard and download buttons to include text

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * Uses a cache-first strategy for static assets.
  */
 
-const CACHE_VERSION = "v97-0e4a45e";
+const CACHE_VERSION = "v99-b264679";
 const STATIC_CACHE_NAME = `sunshine-optimist-static-${CACHE_VERSION}`;
 
 // Static assets to cache on install
