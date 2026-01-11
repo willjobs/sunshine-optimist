06c73f30eadcd5340847e953847366ce332562cf
Will Jobs <7775919+willjobs@users.noreply.github.com>
2026-01-10 20:25:46 -0500

    One more change to feedback icon: white with black outline

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * Uses a cache-first strategy for static assets.
  */
 
-const CACHE_VERSION = "v114-87ee75a";
+const CACHE_VERSION = "v115-b234819";
 const STATIC_CACHE_NAME = `sunshine-optimist-static-${CACHE_VERSION}`;
 
 // Static assets to cache on install
